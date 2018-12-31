57dd24d47107495d2e8ea272e71f5e98e41d93c1
Dhiraj Basukala <dhiraj.basukala@gmail.com>
2018-12-31 18:16:17 +0545

    Few minor fixes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [newCount, setNewCount] = useState('')
   useEffect(()=>{
     document.title = count;
   })
@@ -14,14 +15,27 @@ function App() {
   const  decrement = () => { 
     setCount(count - 1) ; 
   }
-  
+  const getNewCount = (e) => {
+    const myCount = e.target.value *1
+
+    if(myCount > 0 && myCount < 100 || myCount === '' ){
+      setNewCount(myCount);
+    }
+    if(myCount > 0 && myCount < 100 ){
+      setCount(myCount);
+    }
+  }
   return <div className='counter-wrapper'> 
   <div className="counter">
+    
+    <input type="text" placeholder="enter number" value={newCount} className="newCount" onChange ={getNewCount} min="0" max="99" />
+    {/* <button onClick={enterNumber} className="enter-number">  Enter number </button> */}
+
     <div className="number">
       <span>{count}</span></div>
       <div className="actions">
-        <button onClick={decrement} disabled={ count === 0}  className="before">  Before</button>
-        <button onClick={increment} disabled={ count === 99} className="after">After  </button>
+        <button onClick={decrement} disabled={ count === 0}  className="before">  Before <span>{count -1 }</span></button>
+        <button onClick={increment} disabled={ count === 99} className="after">After <span>{count + 1}</span> </button>
       </div>
     </div>
   </div>
